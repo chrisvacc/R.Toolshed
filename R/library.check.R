b1276b7a4e65799b3eb3517b8019c3687cb6031a
chrisvacc <chrisvacc@gmail.com>
2019-01-26 20:04:37 -0500

    Update library.check.R

--- R/library.check.R
+++ R/library.check.R
@@ -2,17 +2,16 @@
 #'
 #' @description Load Library if it exists or install.packages if not
 #' @param package_name the name of the package you want to load
-#' @keywords wd
+#' @keywords library
 #' @export
 #' @examples
 #' packages<-c("ggplot2", "afex", "ez", "Hmisc", "pander", "plyr")
 #' check.packages(packages)
 
 library.check<- function(pkg){
+  options(repos=structure(c(CRAN="http://cloud.r-project.org/")))
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg))
     install.packages(new.pkg, dependencies = TRUE)
   sapply(pkg, require, character.only = TRUE)
 }
-
-
